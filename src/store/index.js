0ccb9f135d13057636fe54c189ddc1df446a5b34
nikukyugamer <takiya@toran.sakura.ne.jp>
2020-08-09 19:18:22 +0900

    vuex-persistedstateの最低限の記述を追加した

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 import rewards from '@/store/rewards'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
   modules: {
     rewards: rewards,
   },
