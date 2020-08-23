ebd4efd96f0ed25e8f1aa7ceac12360ceaee3b77
nikukyugamer <takiya@toran.sakura.ne.jp>
2020-08-24 08:02:30 +0900

    Update the past AMA links

--- src/components/UpdateInfo.vue
+++ src/components/UpdateInfo.vue
@@ -5,20 +5,33 @@
         <div class="column is-12-mobile is-12-tablet is-12-desktop">
           <div class="content">
             <h1 class="has-text-weight-bold is-size-5">更新情報</h1>
-            <span class="has-text-weight-bold">[2020/08/24]</span><br /><span
-              >2020/08/22の「一問一答」をまとめました！</span
+            <span class="has-text-weight-bold">[2020/08/23]</span><br /><span
+              >「一問一答」をまとめました！</span
             >
+            <br />
             <br />
             <a
               href="https://scrapbox.io/100heroesstrong/%E6%B2%B3%E9%87%8E%E7%B4%94%E5%AD%90%E3%81%95%E3%82%93%E3%81%B8%E3%81%AE%E4%B8%80%E5%95%8F%E4%B8%80%E7%AD%94%EF%BC%88%E6%97%A5%E6%9C%AC%E8%AA%9E%E3%83%81%E3%83%A3%E3%83%B3%E3%83%8D%E3%83%AB%EF%BC%89_(2020%2F8%2F22)"
               target="_blank"
-              >日本語チャンネル</a
+              >(2020/08/22) 日本語チャンネル</a
             >
             <br />
             <a
               href="https://scrapbox.io/100heroesstrong/%E6%B2%B3%E9%87%8E%E7%B4%94%E5%AD%90%E3%81%95%E3%82%93%E3%81%B8%E3%81%AE%E4%B8%80%E5%95%8F%E4%B8%80%E7%AD%94%EF%BC%88%E8%8B%B1%E8%AA%9E%E3%83%81%E3%83%A3%E3%83%B3%E3%83%8D%E3%83%AB%EF%BC%89_(2020%2F8%2F23)"
               target="_blank"
-              >英語チャンネル</a
+              >(2020/08/22) 英語チャンネル</a
+            >
+            <br />
+            <a
+              href="https://scrapbox.io/100heroesstrong/%E6%9D%91%E5%B1%B1%E5%90%89%E9%9A%86%E3%81%95%E3%82%93%E3%81%B8%E3%81%AE%E4%B8%80%E5%95%8F%E4%B8%80%E7%AD%94%EF%BC%88%E6%97%A5%E6%9C%AC%E8%AA%9E%E3%83%81%E3%83%A3%E3%83%B3%E3%83%8D%E3%83%AB%EF%BC%89(2020%2F8%2F9)"
+              target="_blank"
+              >(2020/08/09) 日本語チャンネル</a
+            >
+            <br />
+            <a
+              href="https://scrapbox.io/100heroesstrong/%E6%9D%91%E5%B1%B1%E5%90%89%E9%9A%86%E3%81%95%E3%82%93%E3%81%B8%E3%81%AE%E4%B8%80%E5%95%8F%E4%B8%80%E7%AD%94%EF%BC%88%E8%8B%B1%E8%AA%9E%E3%83%81%E3%83%A3%E3%83%B3%E3%83%8D%E3%83%AB%EF%BC%89(2020%2F8%2F9)"
+              target="_blank"
+              >(2020/08/09) 英語チャンネル</a
             >
             <br />
             <br />
