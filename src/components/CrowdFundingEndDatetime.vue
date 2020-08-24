bc7b1dd6d04c8fd6bae4de7dd2f6ae8bb3ee806c
nikukyugamer <takiya@toran.sakura.ne.jp>
2020-08-24 17:53:25 +0900

    Fix minor points

--- src/components/CrowdFundingEndDatetime.vue
+++ src/components/CrowdFundingEndDatetime.vue
@@ -4,11 +4,10 @@
       <div class="columns is-multiline is-mobile is-tablet is-desktop">
         <div class="column is-12-mobile is-12-tablet is-12-desktop">
           <div class="content">
-            <h1 class="has-text-weight-bold is-size-4">
-              クラウドファンディング終了日時
-            </h1>
             <div class="is-size-5">
-              <span>クラウドファンディングの終了日時は</span><br />
+              <span class="has-text-weight-bold"
+                >クラウドファンディングの終了日時は</span
+              ><br />
               <span class="has-text-weight-bold">2020/08/29 (土)</span
               ><br /><span class="has-text-weight-bold">25:00です</span
               ><br /><span class="has-text-weight-bold">（土曜日深夜）</span>
--- src/components/UpdateInfo.vue
+++ src/components/UpdateInfo.vue
@@ -4,9 +4,9 @@
       <div class="columns is-multiline is-mobile is-tablet is-desktop">
         <div class="column is-12-mobile is-12-tablet is-12-desktop">
           <div class="content">
-            <h1 class="has-text-weight-bold is-size-5">更新情報</h1>
+            <h1 class="has-text-weight-bold is-size-5">更新履歴</h1>
             <span class="has-text-weight-bold">[2020/08/23]</span><br /><span
-              >「一問一答」をまとめました！</span
+              >Discordでの「一問一答」をまとめました！！</span
             >
             <br />
             <br />
@@ -41,17 +41,13 @@
             >
             <br />
             <br />
-            <span class="has-text-weight-bold">[2020/08/23]</span><br /><span
-              >最新の情報を反映しました！</span
-            >
-            <br />
-            <br />
             <span class="has-text-weight-bold">[2020/08/19]</span><br /><span
               >「<router-link to="/what-is-add-on#page-top"
                 >アドオン</router-link
               >」のページを追加しました！！</span
             >
             <br />
+            <!-- 上記以前の更新履歴は「こちら」 -->
           </div>
         </div>
       </div>
